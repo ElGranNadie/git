a57f3077060c685b18fee50154fa138b1c157c5b
ElGranNadie <sagainfinidad@gmail.com>
2025-03-31 17:27:49 -0500

    modificacion

--- movil/lib/movil.dart
+++ movil/lib/movil.dart
@@ -1,6 +1,14 @@
+import 'archivo.dart';
+
 void main(){
-  print("Hello, world!");
-  print(calculate());
+    List<List<dynamic>> matrizPreios2 = [
+    //item       ,lun  ,mar  ,mie  ,jue  ,vie  ,sab  ,dom  
+    ["camisas"   ,100  ,90   ,10   ,70   ,60   ,70   ,60  ],
+    ["pantalones",92.0 ,98.2 , 76.5,92.0 ,98.2 ,16.0 ,98.5],
+    ["zapatos"   ,100  ,90   , 80  ,92.0 ,98.2 ,70   ,60  ],
+    ["jeans"     ,100  ,90   , 80  ,92.3 ,98.2 ,70   ,60  ],
+  ];
+  imprimir(matrizPreios2);
 }
 
 int calculate() {
