d232ccaa2596be2728f13d52ea5a9f1d7f514272
ElGranNadie <sagainfinidad@gmail.com>
2025-03-31 17:23:34 -0500

    creacion del archivo.dart

---
+++ movil/lib/archivo.dart
@@ -0,0 +1,46 @@
+void main() {
+  List<List<dynamic>> matrizPrecios = [
+    //item       ,lun  ,mar  ,mie  ,jue  ,vie  ,sab  ,dom  
+    ["camisas"   ,100  ,90   ,80   ,70   ,60   ,70   ,60  ],
+    ["pantalones",92.0 ,98.2 , 76.0,92.0 ,98.2 ,16.0 ,98.5],
+    ["zapatos"   ,100  ,90   , 80  ,92.0 ,98.2 ,70   ,60  ],
+    ["jeans"     ,100  ,90   , 80  ,92.0 ,98.2 ,70   ,60  ],
+  ];
+  //print(promedio(matrizPrecios[0]));
+  //print(alto(matrizPrecios[0]));
+  imprimir(matrizPrecios);
+}
+void imprimir(List<List<dynamic>> estudiantes){
+  int x = 1;
+  for(List<dynamic> notas in estudiantes){
+    print("Producto No.: "+ x.toString());
+    print (" Nombre.: "+ notas[0]);
+    print(" promedio precios: " + promedio(cortar(notas)).toString());
+    print(" precio mas alto: "+ alto(cortar(notas)).toString());
+    x+=1;
+  }
+}
+List<double> cortar(List<dynamic> notas){
+  List<double> aditivo = [];
+  for (int i=1; i<notas.length; i++ ){
+    aditivo.add(notas[i]);
+  }
+  return aditivo;
+}
+double promedio(List<dynamic> notas){
+  double suma=0;
+
+  for (double nota in notas){
+    suma+=nota;
+  }
+  return suma;
+}
+double alto(List<dynamic> notas){
+  double notaMasAlta = notas[0];
+  for (double nota in notas){
+    if(nota>notaMasAlta){
+      notaMasAlta=nota;
+    }
+  }
+  return notaMasAlta;
+}
