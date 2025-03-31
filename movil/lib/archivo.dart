9838af5b17719ad66f97e0598dfbc9dbe75d6807
ElGranNadie <sagainfinidad@gmail.com>
2025-03-31 17:48:47 -0500

    ada

--- movil/lib/archivo.dart
+++ movil/lib/archivo.dart
@@ -1,17 +1,21 @@
-void main() {
-  List<List<dynamic>> matrizPrecios = [
+void algo() {
+    List<List<dynamic>> matrizPreios2 = [
     //item       ,lun  ,mar  ,mie  ,jue  ,vie  ,sab  ,dom  
-    ["camisas"   ,100.0,90.0 ,80.0 ,70.0 ,60.0 ,70.0 ,60.0  ],
-    ["pantalones",92.0 ,98.2 ,76.0 ,92.0 ,98.2 ,16.0 ,98.5],
-    ["zapatos"   ,100.0,90.0 ,80.0 ,92.0 ,98.2 ,70.0 ,60.0  ],
-    ["jeans"     ,100.0,90.0 ,80.0 ,92.0 ,98.2 ,70.0 ,60.0  ],
+    ["camisas"   ,100.3,90.2 ,81.4 ,77.4 ,60.0 ,70.0 ,60.0  ],
+    ["pantalones",92.0 ,98.2 ,76.0 ,92.6 ,98.2 ,16.0 ,98.0  ],
+    ["zapatos"   ,100.5,90.6 ,80.0 ,92.0 ,98.4 ,70.7 ,60.9  ],
+    ["blusas"    ,95.0 ,85.0 ,75.0 ,70.0 ,65.0 ,55.0 ,50.0  ],
+    ["faldas"    ,88.0 ,78.0 ,68.0 ,58.0 ,48.0 ,38.0 ,28.0  ],
+    ["chaquetas" ,120.0,110.0,100.0,90.0 ,80.0 ,70.0 ,60.0  ],
+    ["sombreros" ,50.0 ,45.0 ,40.0 ,35.0 ,30.0 ,25.0 ,20.0  ],
+    ["jeans"     ,100.7,90.7 ,80.0 ,92.0 ,98.2 ,70.6 ,60.8  ],
   ];
   //print(promedio(matrizPrecios[0]));
   //print(alto(matrizPrecios[0]));
-  imprimir(matrizPrecios);
+  imprimir(matrizPreios2);
 }
 void imprimir(List<List<dynamic>> estudiantes){
-  double x = 1;
+  int x = 1;
   for(List<dynamic> notas in estudiantes){
     print("Producto No.: "+ x.toString());
     print (" Nombre.: "+ notas[0]);
--- movil/lib/movil.dart
+++ movil/lib/movil.dart
@@ -1,14 +1,7 @@
 import 'archivo.dart';
 
 void main(){
-    List<List<dynamic>> matrizPreios2 = [
-    //item       ,lun  ,mar  ,mie  ,jue  ,vie  ,sab  ,dom  
-    ["camisas"   ,100.3,90.2 ,81.4 ,77.4 ,60.0 ,70.0 ,60.0  ],
-    ["pantalones",92.0 ,98.2 ,76.0 ,92.6 ,98.2 ,16.0 ,98.0  ],
-    ["zapatos"   ,100.5,90.6 ,80.0 ,92.0 ,98.4 ,70.7 ,60.9  ],
-    ["jeans"     ,100.7,90.7 ,80.0 ,92.0 ,98.2 ,70.6 ,60.8  ],
-  ];
-  imprimir(matrizPreios2);
+  algo();  
 }
 
 int calculate() {
