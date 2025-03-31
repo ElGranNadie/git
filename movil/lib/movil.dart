c7f79e731823f7229d36cc26e5ecdf2dc77e78fd
ElGranNadie <sagainfinidad@gmail.com>
2025-03-31 17:29:28 -0500

    a

--- movil/lib/movil.dart
+++ movil/lib/movil.dart
@@ -4,9 +4,9 @@ void main(){
     List<List<dynamic>> matrizPreios2 = [
     //item       ,lun  ,mar  ,mie  ,jue  ,vie  ,sab  ,dom  
     ["camisas"   ,100  ,90   ,10   ,70   ,60   ,70   ,60  ],
-    ["pantalones",92.0 ,98.2 , 76.5,92.0 ,98.2 ,16.0 ,98.5],
-    ["zapatos"   ,100  ,90   , 80  ,92.0 ,98.2 ,70   ,60  ],
-    ["jeans"     ,100  ,90   , 80  ,92.3 ,98.2 ,70   ,60  ],
+    ["pantalones",92   ,981 , 76.0 ,920  ,982  ,16.0 ,985 ],
+    ["zapatos"   ,100  ,90   , 80  ,920  ,982  ,70   ,60  ],
+    ["jeans"     ,100  ,90   , 80  ,921  ,982  ,70   ,60  ],
   ];
   imprimir(matrizPreios2);
 }
